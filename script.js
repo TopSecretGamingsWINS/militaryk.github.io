83c0dc0badc3ecd3962cd44ea4f036b06d1646da
militaryk <38089040+militaryk@users.noreply.github.com>
2018-06-06 08:17:00 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function onSignIn(googleUser) {
+function onSignIn(googleUser) 
+{
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+function onSingIn(googleUser)
+{
+var profile=googleUser.getBasicProfile();
+  $("g.sign-in2").css("display","none");
+  $("g.data").css("display","block");
+  $("#pic").attr('src',profile.getImageUrl());
+  $("#email").text('src',profile.getEmail());
 }
