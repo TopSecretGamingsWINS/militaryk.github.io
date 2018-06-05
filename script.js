14e84f5267eae4d47c0c981fb744d4ded758e7f0
militaryk <38089040+militaryk@users.noreply.github.com>
2018-06-05 19:18:11 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-fuction onSignIn(googleUser)
-{
-    var profile=googleUser.getBasicProfile();
-    $(".g-signin2").css("display","none");
-    $(".data").css("display","block");
-    $("#pic").attr('src',profile.getImageUrl());
-    $("#email").text('src',profile.getEmaill());
-    
-}
-    
+<a href="#" onclick="signOut();">Sign out</a>
+<script>
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+</script>
