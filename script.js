6fda46fa0e382e40708bd37eed6a97209d7fa9bc
militaryk <38089040+militaryk@users.noreply.github.com>
2018-06-07 12:02:34 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function onSingIn(googleUser)
 {
 var profile=googleUser.getBasicProfile();
-  $(".g.signin2").css("display","none");
+  $(".g-signin2").css("display","none");
   $(".data").css("display","block");
   $("#pic").attr('src',profile.getImageUrl());
   $("#email").text('src',profile.getEmail());
